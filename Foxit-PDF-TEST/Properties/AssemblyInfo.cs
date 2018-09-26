1c4ae15f01412e5cd8c1611db1ee2f3f8a46eaf0
刘晨 <chenliu6@iflytek.com>
2018-09-26 19:52:48 +0800

    重新调整Foxit-PDF-TEST

--- Foxit-PDF-TEST/Properties/AssemblyInfo.cs
+++ Foxit-PDF-TEST/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("fd16f5a5-442f-4ed4-aea8-7ff4127afaec")]
+[assembly: Guid("2dec51de-7424-430b-b33f-53b819c0ab68")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      修订号
 //
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
-//通过使用 "*"，如下所示:
+// 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
